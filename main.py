a5a29b6344d00f9be7aeecf63fd6d60a0471ff7b
Zeeshan Ahmed <zeeshan060801@gmail.com>
2023-04-07 22:37:43 +0530

    platform gen in progress

--- main.py
+++ main.py
@@ -14,6 +14,10 @@ FRIC = -0.25
 FPS = 60
 GRAVITY = 0.5
 VMAX = 4
+JUMP_SPEED = 15
+JUMP_HEIGHT = JUMP_SPEED**2 / (2 * GRAVITY * FPS)
+PLAYER_HEIGHT = 30
+PLATFORM_HEIGHT = 10
 
 FramePerSec = pygame.time.Clock()
 
@@ -28,14 +32,13 @@ class Player(pygame.sprite.Sprite):
         self.surf = pygame.Surface((30, 30))
         self.surf.fill((255, 255, 0))
         self.rect = self.surf.get_rect()
-        self.rect.center = (WIDTH / 2, HEIGHT - 20)
-
         self.vel = vec(0, 0)
         self.gravity = vec(0, GRAVITY)
         self.jumping = False
         self.score = 0
         self.flipped = False
         self.collided_platform = None
+        self.pos = vec(WIDTH // 2, HEIGHT - 20)
 
     def move(self):
         # handle collisions
@@ -44,20 +47,20 @@ class Player(pygame.sprite.Sprite):
             if self.vel.y < 0:
                 # going up, I can only collide from below
                 for collided_platform in hits:
-                    self.rect.top = collided_platform.rect.bottom + 1
+                    # set the player to top of the platform ig
+                    self.pos.y = collided_platform.rect.bottom - PLATFORM_HEIGHT
                     self.vel.y = 0
 
             elif self.vel.y > 0:
                 # going down, I can only collide from above
                 for collided_platform in hits:
-                    self.rect.bottom = collided_platform.rect.top + 1
+                    self.pos.y = collided_platform.rect.top + 1
                     self.vel.y = 0
                     self.jumping = False
                     self.collided_platform = collided_platform
             else:
                 pass
 
-        
         pressed_keys = pygame.key.get_pressed()
 
         self.vel = vec(0, self.vel.y)
@@ -67,12 +70,12 @@ class Player(pygame.sprite.Sprite):
             self.vel.x = -PLAYER_HORIZONTAL_VEL
         if pressed_keys[K_RIGHT]:
             self.vel.x = PLAYER_HORIZONTAL_VEL
-        self.vel.x *= (-1 if self.flipped else 1)
-        
+        self.vel.x *= -1 if self.flipped else 1
+
         if pressed_keys[K_SPACE] and len(hits) > 0 and not self.jumping:
             # jumping
             self.jumping = True
-            self.vel.y = -15
+            self.vel.y = -JUMP_SPEED
             self.collided_platform = None
 
         for event in pygame.event.get():
@@ -86,21 +89,18 @@ class Player(pygame.sprite.Sprite):
         if self.collided_platform:
             self.vel.x += self.collided_platform.vel.x
 
-        pos = self.rect.midbottom     
-        pos += self.vel
-        if pos.x > WIDTH:
-            pos.x = 0
-        if pos.x < 0:
-            pos.x = WIDTH
-
-        self.rect.midbottom = pos
+        self.pos += self.vel
+        if self.pos.x > WIDTH:
+            self.pos.x = 0
+        if self.pos.x < 0:
+            self.pos.x = WIDTH
 
     def cancel_jump(self):
         if self.jumping:
             if self.vel.y < -3:
                 self.vel.y = -3
 
-    def gonnaFlip(self):
+    def gonna_flip(self):
         self.surf.fill((255, 255, 255))
 
     def flip(self):
@@ -111,28 +111,42 @@ class Player(pygame.sprite.Sprite):
             self.surf.fill((255, 255, 0))
         self.gravity = vec(0, GRAVITY * (-1 if self.flipped else 1))
 
+    def update_rect(self):
+        self.rect.midbottom = int(self.pos.x), int(self.pos.y)
 
-class platform(pygame.sprite.Sprite):
-    def __init__(self, moving=True):
+
+class Platform(pygame.sprite.Sprite):
+    def __init__(self, size=None, position=None, moving=True):
         super().__init__()
-        self.surf = pygame.Surface((random.randint(50, 100), 12))
-        self.surf.fill((0, 255, 0))
-        self.rect = self.surf.get_rect(
-            center=(random.randint(0, WIDTH - 10), random.randint(0, HEIGHT - 30))
-        )
-        if moving:
-          self.vel = vec(random.choice([1, -1]) * 2, 0)
+        if size is None:
+            self.surf = pygame.Surface((random.randint(50, 100), PLATFORM_HEIGHT))
         else:
-          self.vel = vec(0, 0)
+            self.surf = pygame.Surface(size)
+
+        # color
+        self.surf.fill((0, 255, 0))
+        self.rect = self.surf.get_rect()
+        if position is None:
+            self.pos = vec(
+                (random.randint(0, WIDTH - 10), random.randint(0, HEIGHT - 30))
+            )
+        else:
+            self.pos = vec(position)
+        if moving:
+            self.vel = vec(random.choice([1, -1]) * 2, 0)
+        else:
+            self.vel = vec(0, 0)
         self.moving = moving
         self.point = True
 
     def move(self):
-        self.rect.move_ip(self.vel)
-        if self.vel.x > 0 and self.rect.left > WIDTH:
-            self.rect.right = 0
-        if self.vel.x < 0 and self.rect.right < 0:
-            self.rect.left = WIDTH
+        self.pos += self.vel
+        half_width = self.rect.width / 2
+        if self.vel.x > 0 and self.pos.x + half_width > WIDTH:
+            self.pos.x = -half_width
+        if self.vel.x < 0 and self.pos.x - half_width < 0:
+            self.pos.x = WIDTH + half_width
+        self.rect.center = int(self.pos.x), int(self.pos.y)
 
 
 def check(platform, groupies):
@@ -146,15 +160,17 @@ def check(platform, groupies):
                 abs(platform.rect.bottom - entity.rect.top) < 40
             ):
                 return True
-        C = False
+
 
 P1 = Player()
 all_sprites = pygame.sprite.Group()
 platforms = pygame.sprite.Group()
+top_platforms = []
 
 # GAME STATES
 INVERSE = False
 flipIn = 300
+
 
 def flip_state():
     global INVERSE
@@ -162,20 +178,22 @@ def flip_state():
     INVERSE = not INVERSE
 
 
-def init_platform(count):
-    PT1 = platform(False)
-    PT1.rect.center = (WIDTH / 2, HEIGHT - 10)
-    PT1.moving = False
+def add_platforms():
+    """
+    Uses top_platforms to decide next layer.
+    """
+    pl = Platform()
+    platforms.add(pl)
+    all_sprites.add(pl)
+
+
+def init_platform():
+    PT1 = Platform(position=(WIDTH // 2, HEIGHT - 10), moving=False)
     platforms.add(PT1)
     all_sprites.add(PT1)
+    top_platforms.append(PT1)
 
-    for _ in range(count):
-        pl = platform()
-        while check(pl, platforms):
-            pl = platform()
-
-        platforms.add(pl)
-        all_sprites.add(pl)
+    add_platforms()
 
 
 def keyboard_events():
@@ -206,23 +224,23 @@ def end_game():
 
 
 def shift_level_up():
-    P1.rect.bottom += abs(P1.vel.y)
+    P1.pos.y += abs(P1.vel.y)
     for plat in platforms:
-        plat.rect.y += abs(P1.vel.y)
-        if plat.rect.top >= HEIGHT:
+        plat.pos.y += abs(P1.vel.y)
+        if plat.pos.y >= HEIGHT:
             plat.kill()
 
 
 def main():
     global flipIn
     all_sprites.add(P1)
-    init_platform(20)
+    init_platform()
 
     while True:
         flipIn -= 1
 
         if flipIn == 60:
-            P1.gonnaFlip()
+            P1.gonna_flip()
 
         if flipIn == 0:
             flip_state()
@@ -236,6 +254,8 @@ def main():
 
         if P1.rect.top <= HEIGHT / 3:
             shift_level_up()
+
+        P1.update_rect()
 
         displaysurface.fill((0, 0, 0))
         f = pygame.font.SysFont("Verdana", 20)
